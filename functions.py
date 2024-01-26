06d2ae46711b7759d253b4f436e8d7f4d7d791d6
Mike Guilmette <mguilmette@gmail.com>
2024-01-26 10:33:43 -0500

    Added Team Names

--- functions.py
+++ functions.py
@@ -32,6 +32,17 @@ def teamSelection():
     for i in range(30):
         teams.append(str(i+1))
 
+    print('1.  Atlanta       2.  Boston        3.  Brooklyn')
+    print('4.  Charlotte     5.  Chicago       6.  Cleveland')
+    print('7.  Dallas        8.  Denver        9.  Detroit')
+    print('10. Golden State  11. Houston       12. Indiana')
+    print('13. LA Clippers   14. LA Lakers     15. Memphis')
+    print('16. Miami         17. Milwaukee     18. Minnesota')
+    print('19. New Orleans   20. New York      21. Oklahoma City')
+    print('22. Orlando       23. Philadelphia  24. Phoenix')
+    print('25. Portland      26. Sacramento    27. San Antonio')
+    print('28. Toronto       29. Utah          30. Washington')
+    
     while answer not in teams:
         answer = input('Pick Team:\n')
     if answer == '1':
